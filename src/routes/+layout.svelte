370dc259356f88615541bf28956a020b30ecbe30
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-05-23 00:00:33 +0300

    с

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+	const currentYear = new Date().getFullYear();
 </script>
 
 <header class="bg-gray-900">
@@ -115,11 +116,16 @@
 
 <footer class="border-t border-gray-100 bg-gray-900 py-6">
 	<div class="mx-auto max-w-7xl px-6 lg:px-8">
-		<div class="flex justify-center">
-			<a
-				href="mailto:privacy@bonus5.ru"
-				class="text-sm font-semibold text-gray-400 hover:text-white">privacy@bonus5.ru</a
-			>
+		<div class="flex items-center justify-between">
+			<div>
+				<p class="m-0 text-sm text-gray-400">© {currentYear} BONUS5. Все права защищены.</p>
+			</div>
+			<div>
+				<a
+					href="mailto:privacy@bonus5.ru"
+					class="text-sm font-semibold text-gray-400 hover:text-white">privacy@bonus5.ru</a
+				>
+			</div>
 		</div>
 	</div>
 </footer>
