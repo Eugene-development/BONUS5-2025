526c89f236c397acda8d53959388e8acece6417d
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-05-23 21:21:19 +0300

    c

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
 	import '../app.css';
 	import { Menu } from './layout/header/UI';
+	import { Component } from './layout/footer/UI';
 	let { children } = $props();
-	const currentYear = new Date().getFullYear();
 </script>
 
 <header class="bg-gray-900">
@@ -12,17 +12,5 @@
 {@render children()}
 
 <footer class="border-t border-gray-100 bg-gray-900 py-6">
-	<div class="mx-auto max-w-7xl px-6 lg:px-8">
-		<div class="flex items-center justify-between">
-			<div>
-				<p class="m-0 text-sm text-gray-400">© {currentYear} BONUS5. Все права защищены.</p>
-			</div>
-			<div>
-				<a
-					href="mailto:privacy@bonus5.ru"
-					class="text-sm font-semibold text-gray-400 hover:text-white">privacy@bonus5.ru</a
-				>
-			</div>
-		</div>
-	</div>
+	<Component />
 </footer>
---
+++ src/routes/layout/footer/UI/Component/index.svelte
@@ -0,0 +1,17 @@
+<script>
+	const currentYear = new Date().getFullYear();
+</script>
+
+<div class="mx-auto max-w-7xl px-6 lg:px-8">
+	<div class="flex items-center justify-between">
+		<div>
+			<p class="m-0 text-sm text-gray-400">© {currentYear} BONUS5. Все права защищены.</p>
+		</div>
+		<div>
+			<a
+				href="mailto:privacy@bonus5.ru"
+				class="text-sm font-semibold text-gray-400 hover:text-white">privacy@bonus5.ru</a
+			>
+		</div>
+	</div>
+</div>
