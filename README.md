74e93ee17b3456727e5914ebb4ea01d84e48bf37
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-06-04 22:43:49 +0300

    с

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-docker build -t larux/bonus5:230525 . && docker push larux/bonus5:230525
+docker build -t larux/bonus5:280525 . && docker push larux/bonus5:280525
--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -2,5 +2,13 @@
 	import { Main, Users } from './UI';
 </script>
 
+<svelte:head>
+	<title>Платформа Bonus5 – экономьте и зарабатывайте</title>
+	<meta
+		name="description"
+		content="Платформа Bonus5 — сервис для экономии и заработка: получайте бонусы за покупки, участвуйте в акциях и используйте выгодные предложения от партнеров."
+	/>
+</svelte:head>
+
 <Main />
 <Users />
